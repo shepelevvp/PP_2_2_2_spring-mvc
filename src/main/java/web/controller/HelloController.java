cc8d236d5b9f0a14a15ee96296d90b2135146289
Shepelevvp <shepelevvp@gmail.com>
2024-12-19 21:34:34 +0300

    Initial commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -11,7 +11,8 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
+
+	@GetMapping("/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
